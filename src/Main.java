b8352f2e44708ed22cfbf30d78e847b5fbbf3311
s27869 <s27869@pjwstk.edu.pl>
2024-10-08 09:00:40 +0200

    task delegation specification

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TODO: We need to add missing classes!
 
+// OK, I will add `Adder` and s28792 will add `Subtractor`.
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
